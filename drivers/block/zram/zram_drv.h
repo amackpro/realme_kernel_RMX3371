33ac912ff157a18a9cf49abce6b2baeb585a722a
amackpro <prajwal.kumar008@gmail.com>
2023-04-20 18:07:06 +0530

    treewide: enable and fix some drivers

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -80,6 +80,7 @@ struct zram_table_entry {
 	union {
 		struct zram_entry *entry;
 		unsigned long element;
+		unsigned long blk_idx;
 #ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
 		unsigned long page;
 #endif
@@ -152,10 +153,7 @@ struct zram {
 	spinlock_t wb_limit_lock;
 	bool wb_limit_enable;
 	u64 bd_wb_limit;
-	struct block_device *bdev;
-	unsigned int old_block_size;
 	unsigned long *bitmap;
-	unsigned long nr_pages;
 #endif
 #ifdef CONFIG_ZRAM_MEMORY_TRACKING
 	struct dentry *debugfs_dir;
